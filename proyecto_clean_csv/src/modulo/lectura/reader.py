f665678d007bebd0f856be4b63cd93a1238c836b
oumaima <oma73k@gmail>
2026-02-10 20:37:11 +0100

    modificacion reader comentada

--- proyecto_clean_csv/src/modulo/lectura/reader.py
+++ proyecto_clean_csv/src/modulo/lectura/reader.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from typing import Generator, Dict, Any
+#from contextlib import contextmanager
 
 path = "../../../examples/retai_store_sales.csv"
 def reader(path: str) -> pd.DataFrame:
@@ -17,4 +18,18 @@ def reader(path: str) -> pd.DataFrame:
 #        print(f"Erro al leer el CSV:{e}")
 #        return None
 
-# def generador_csv(path: str) -> Generator[Dict[str,Any], None, None]:
+
+#@contextmanager
+#def abrir_archivo(path: str):
+#        f = open(path, "r", encoding="utf-8", errors="replace")
+#       try:
+#                yield f
+#        finally:
+#                f.close() 
+
+#def generador_csv(path: str) -> Generator[Dict[str,Any], None, None]:
+#        with abrir_archivo(path) as f:
+#                reader=csv.DictReader(f)
+
+#                for fila in reader:
+#                        yield fila
