9359f99f4d65207258b59a2cd87993b1249f5045
Cecilia <820417@unizar.es>
2026-02-16 11:31:32 +0100

    decorator

--- proyecto_clean_csv/src/module/reports/decorators.py
+++ proyecto_clean_csv/src/module/reports/decorators.py
@@ -1,62 +1,55 @@
-import pandas as pd
 import logging
 import time
-from typing import Optional, Any
 from collections.abc import Callable
+from typing import Any, Optional
+
+import pandas as pd
+
+logger = logging.getLogger(__name__)
 
 
-logger=logging.getLogger(__name__)
-
-def track_changes(_func: Callable | None=None,*,action: str|None=None):
+def track_changes(_func: Callable | None = None, *, action: str | None = None):
 
     def decorator(func: Callable):
         def wrapper(*args, **kwargs):
-            start=time.perf_counter()
+            start = time.perf_counter()
 
             df_antes = next((arg for arg in args if isinstance(arg, pd.DataFrame)), None)
             filas_antes = len(df_antes) if df_antes is not None else 0
 
             if action:
                 logger.info(
-                    "Se inicia la ejecución de la función '%s'. Acción: %s.",
-                    func.__name__,
-                    action
+                    "Se inicia la ejecución de la función '%s'. Acción: %s.", func.__name__, action
                 )
             else:
-                logger.info(
-                    "Se inicia la ejecución de la función '%s'.",
-                    func.__name__
-                )
+                logger.info("Se inicia la ejecución de la función '%s'.", func.__name__)
             result = func(*args, **kwargs)
 
-            duration = time.perf_counter()-start
+            duration = time.perf_counter() - start
 
             if isinstance(result, pd.DataFrame) and filas_antes is not None:
                 filas_despues = len(result)
                 diff = filas_antes - filas_despues
                 if diff > 0:
                     logger.info(
-                        "La función '%s' ha finalizado. " \
+                        "La función '%s' ha finalizado. "
                         "Se han eliminado %d filas (antes: %d, después: %d).",
                         func.__name__,
                         diff,
                         filas_antes,
-                        filas_despues
+                        filas_despues,
                     )
                 elif diff == 0:
                     logger.info(
-                        "La función '%s' ha finalizado " \
+                        "La función '%s' ha finalizado "
                         "sin cambios en el número de filas (total: %d).",
                         func.__name__,
-                        filas_despues
-                        )
-            logger.info(
-                 "Tiempo de ejecución de '%s': %.4f segundos.",
-                func.__name__,
-                duration
-                )
+                        filas_despues,
+                    )
+            logger.info("Tiempo de ejecución de '%s': %.4f segundos.", func.__name__, duration)
 
             return result
+
         return wrapper
 
     if _func is None:
@@ -64,30 +57,24 @@ def track_changes(_func: Callable | None=None,*,action: str|None=None):
     return decorator(_func)
 
 
-def track_dtype_changes(func: Callable)-> Callable:
+def track_dtype_changes(func: Callable) -> Callable:
 
     def wrapper(*args, **kwargs):
-        df_antes=next((arg for arg in args if isinstance(arg, pd.DataFrame)), None)
+        df_antes = next((arg for arg in args if isinstance(arg, pd.DataFrame)), None)
 
-        tipos_antes=df_antes.dtypes.to_dict()
+        tipos_antes = df_antes.dtypes.to_dict()
 
-        result=func(*args, **kwargs)
+        result = func(*args, **kwargs)
 
-        tipos_despues=result.dtypes.to.dict()
+        tipos_despues = result.dtypes.to_dict()
 
-        logger.info(
-            "Resumen de cambios de tipos en la función '%s':",
-            func.__name__
-        )
+        logger.info("Resumen de cambios de tipos en la función '%s':", func.__name__)
         for col in tipos_despues:
             tipo_antes = tipos_antes.get(col, "No existía")
             tipo_despues = tipos_despues[col]
 
             logger.info(
-                "Columna '%s' → Tipo antes: %s → Tipo después: %s",
-                col,
-                tipo_antes,
-                tipo_despues
+                "Columna '%s' → Tipo antes: %s → Tipo después: %s", col, tipo_antes, tipo_despues
             )
 
         return result
